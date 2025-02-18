fedbb0e650cd23feacb9c4fda76ba338f959f22f
YashKshirsagar1 <127996689+YashKshirsagar1@users.noreply.github.com>
2025-02-18 15:28:41 +0000

    w

--- Basics/Exercise/3_numbers/3_numbers_work.py
+++ Basics/Exercise/3_numbers/3_numbers_work.py
@@ -1,11 +1,1 @@
-length = 92
-width = 48.8
-area = length * width
-print(area)
-
-num_packets=9
-cost_per_packet=1.49
-total_cost=num_packets*cost_per_packet
-money_paid=20
-cash_back=money_paid-total_cost
-print("Cash back:",cash_back)
+print("hello")
